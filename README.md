6fdc5f34a7308f7a5c8d192ca6965e303a0175a1
Ku-Be <>
2023-10-05 22:42:59 +0200

    Using ZigEmbeddedGroup uf2 now. Doesn't work.

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 Testing how to use zig to generate code for the rp2040.
 It can now boot from ram into a minimal blinky program, written in assembly language, by Matt Jacobson:
 https://github.com/mhjacobson/pico-blink-asm
+
+For now, while the zig package manager does not support local packages, I will not use it.
+Rather, dependencies will be placed into this tree and added as anonymousDependency in build.zig.
+
+The deps directory contains https://github.com/ZigEmbeddedGroup/uf2 .
+The import in build.zig looks like this: `const uf2 = @import("deps/uf2/build.zig");`
+Git submodules can be used for that, but I do not know how those work, so I will add the files manually for now.
+
+In the beginning the elf2uf2 python program by doug summerville https://github.com/dougsummerville/elf2uf2 was used to generate uf2 files.
+Now the uf2 program by ZigEmbeddedGroup is used, as it integrates nicely with the build system.
+
+However, at the moment, this does not work.
+elf2uf2.exe fails with an error "missing arg: --output-path".
+This might be because the output path is directly concatenated onto the flag without a space:
+
+```C:\wdir\zig_micro\zig-asm-standalone\zig-cache\o\a436d06b8e3b94bd4ab1aa1490293233\elf2uf2.exe --family-id RP2040 --elf-path C:\wdir\zig_micro\zig-asm-standalone\zig-cache\o\e45486fe662c1db6d691d4bba282ba05\test.elf --output-pathC:\wdir\zig_micro\zig-asm-standalone\zig-cache\o\79b1fff4b94b645ab40cadf92d1f4ef0\test.uf2```
+
+
--- build.zig
+++ build.zig
@@ -1,73 +1,25 @@
 const std = @import("std");
+const uf2 = @import("deps/uf2/build.zig");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) !void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    // const target = b.standardTargetOptions(.{});
-    var target = try std.zig.CrossTarget.parse(.{.arch_os_abi="arm-freestanding-eabi"});
-    target.cpu_model = .{.explicit = &std.Target.arm.cpu.cortex_m0plus};
+    var target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "arm-freestanding-eabi" });
+    target.cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m0plus };
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "build",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+        .name = "test.elf",
         .root_source_file = undefined,
         .target = target,
         .optimize = optimize,
     });
-    exe.addAssemblyFile(.{.path="src/ramboot.s"});
-    exe.setLinkerScript(.{.path="src/link.ld"});
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    exe.addAssemblyFile(.{ .path = "src/ramboot.s" });
+    exe.setLinkerScript(.{ .path = "src/link.ld" });
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    // const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    // run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    // if (b.args) |args| {
-    //    run_cmd.addArgs(args);
-    // }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    // const unit_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/main.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_unit_tests.step);
+    const uf2_dep = b.anonymousDependency("deps/uf2", uf2, .{});
+    //const uf2_dep = b.dependency("uf2",.{}); not using the package manager, so this does not work.
+    const uf2_file = uf2.from_elf(uf2_dep, exe, .{ .family_id = .RP2040 });
+    const uf2_install = b.addInstallFile(uf2_file, "bin/test.uf2");
+    b.getInstallStep().dependOn(&uf2_install.step);
 }
